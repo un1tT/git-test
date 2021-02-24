72cc788d3a462867f12126694a3b765543bc59d0
Vasiliy Shchukin <unit2wall@gmail.com>
2021-02-24 12:11:32 +0700

    naming change in collections lab

--- collections.ts
+++ collections.ts
@@ -1,16 +1,17 @@
 const findMostFrequentElement = (array: number[]): string => {
     let result = 0;
-    let resultFoundTimes = 0;
+    let resultFound = 0;
 
     array.forEach((value) => {
         const amount = array.filter((num) => num === value).length;
-        if (amount > resultFoundTimes) {
-            resultFoundTimes = amount;
+
+        if (amount > resultFound) {
+            resultFound = amount;
             result = value;
         }
     });
 
-    return `The most frequent number in the array is ${result}, it is found ${resultFoundTimes} times`;
+    return `The most frequent number in the array is ${result}, it is found ${resultFound} times`;
 }
 
 console.log(findMostFrequentElement([7, 2, 2, 2, 3, 7, 7, 7, 7, 3, 5, 9, 7]));

cce6e79270c89a45c82d4b30782e9316a4c268ec
Vasiliy Shchukin <unit2wall@gmail.com>
2021-03-04 04:23:20 +0700

    Provide additional collections solution using loops

--- collections.js
+++ collections.js
@@ -1,10 +1,5 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
-};
 var findMostFrequentElement = function (array) {
-    var uniqueElements = new Set(__spreadArray([], array));
+    var uniqueElements = new Set(array.slice());
     var result = 0;
     var resultFound = 0;
     uniqueElements.forEach(function (value) {
@@ -16,4 +11,24 @@ var findMostFrequentElement = function (array) {
     });
     return "The most frequent number in the array is " + result + ", it is found " + resultFound + " times";
 };
-console.log(findMostFrequentElement([7, 2, 2, 2, 3, 7, 7, 7, 7, 3, 5, 9, 7]));
+var findElementWithLoops = function (array) {
+    var uniqueSet = new Set(array);
+    var result = 0;
+    var resultFound = 0;
+    for (var _i = 0, _a = new Array(...uniqueSet); _i < _a.length; _i++) {
+        var value = _a[_i];
+        var amount = 0;
+        for (var _b = 0; _b < array.length; _b++) {
+            var num = array[_b];
+            (num === value) && amount++;
+        }
+        if (amount > resultFound) {
+            resultFound = amount;
+            result = value;
+        }
+    }
+    return "The most frequent number in the array is " + result + ", it is found " + resultFound + " times";
+};
+var TEST_ARRAY = [7, 2, 2, 2, 3, 7, 7, 7, 7, 3, 5, 9, 7];
+console.log('USING ARRAY METHODS: ', findMostFrequentElement(TEST_ARRAY));
+console.log('USING LOOPS: ', findElementWithLoops(TEST_ARRAY));
--- collections.ts
+++ collections.ts
@@ -16,4 +16,28 @@ const findMostFrequentElement = (array: number[]): string => {
     return `The most frequent number in the array is ${result}, it is found ${resultFound} times`;
 }
 
-console.log(findMostFrequentElement([7, 2, 2, 2, 3, 7, 7, 7, 7, 3, 5, 9, 7]));
+const findElementWithLoops = (array: number[]): string => {
+    const uniqueSet = new Set(array);
+    let result = 0;
+    let resultFound = 0;
+
+    for (const value of new Array(...uniqueSet)) {
+        let amount = 0;
+
+        for (const num of array) {
+           (num === value) && amount++;
+        }
+
+        if (amount > resultFound) {
+            resultFound = amount;
+            result = value;
+        }
+    }
+
+    return `The most frequent number in the array is ${result}, it is found ${resultFound} times`;
+}
+
+const TEST_ARRAY = [7, 2, 2, 2, 3, 7, 7, 7, 7, 3, 5, 9, 7];
+
+console.log('USING ARRAY METHODS: ', findMostFrequentElement(TEST_ARRAY));
+console.log('USING LOOPS: ', findElementWithLoops(TEST_ARRAY));

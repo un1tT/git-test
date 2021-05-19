9e4c694dcfd5ea4a275ec66c9179b3e524f206cf
Vasiliy Shchukin <unit2wall@gmail.com>
2021-05-20 03:50:16 +0700

    Move custom pipe to the separate folder

--- Coloring/src/app/app.module.ts
+++ Coloring/src/app/app.module.ts
@@ -11,7 +11,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EntityFormComponent } from './entity-form/entity-form.component';
-import { ToPixelsPipe } from './to-pixels.pipe';
+import { ToPixelsPipe } from './pipes/to-pixels.pipe';
 
 @NgModule({
   declarations: [
--- Coloring/src/app/to-pixels.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToPixelsPipe } from './to-pixels.pipe';
-
-describe('ToPixelsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToPixelsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
